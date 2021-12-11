6b724d2beb4f00f85c6c743a3995aa66eba13e3c
Emilie johnsen <eaaebjoh@students.eaaa.dk>
2021-12-11 10:35:01 +0100

    11/12 Emilie

--- javascript/index-java.js
+++ javascript/index-java.js
@@ -1,29 +1,31 @@
-const hover1 = document.getElementById("hover1")
+const hover1 = document.getElementById("hover1") // Identificerer elementer med id "hover"
 const hover2 = document.getElementById("hover2")
 const hover3 = document.getElementById("hover3")
 const hover4 = document.getElementById("hover4")
 
-const navigation1 = document.getElementById("navigation1")
+const navigation1 = document.getElementById("navigation1") // Identificerer elementer med id "navigation"
 const navigation2 = document.getElementById("navigation2")
 const navigation3 = document.getElementById("navigation3")
 const navigation4 = document.getElementById("navigation4")
 
-const popup1 = document.getElementById("popup1")
+const popup1 = document.getElementById("popup1") // Identificerer elementer med id "popup"
 const popup2 = document.getElementById("popup2")
 const popup3 = document.getElementById("popup3")
 const popup4 = document.getElementById("popup4")
 
-navigation1.addEventListener ("mouseover", function(){
-    hover1.classList.add("hover-farve");
-    popup1.classList.add("popup-farve");
+// Javascript til navigationen på siden
+
+navigation1.addEventListener ("mouseover", function(){ // Siden "lytter" efter om brugeren har musen på navigation 1
+    hover1.classList.add("hover-farve"); // Når brugeren har mouseover tilføjes class "hover-farve"
+    popup1.classList.add("popup-farve"); // Når brugeren har mouseover tilføjes class "popup-farve"
 })
 
-navigation1.addEventListener ("mouseout", function(){
-    hover1.classList.remove("hover-farve");
-    popup1.classList.remove("popup-farve");
+navigation1.addEventListener ("mouseout", function(){ // Siden "lytter" efter om brugeren har musen væk fra navigation 1
+    hover1.classList.remove("hover-farve"); // Når brugeren har mouseout fjernes class "hover-farve"
+    popup1.classList.remove("popup-farve"); // Når brugeren har mouseout fjernes class "popup-farve"
 })
 
-navigation2.addEventListener ("mouseover", function(){
+navigation2.addEventListener ("mouseover", function(){ // Samme funktioner på alle navigationsknapper
     hover2.classList.add("hover-farve");  
     popup2.classList.add("popup-farve");
 })
--- javascript/proces-java.js
+++ javascript/proces-java.js
@@ -23,3 +23,91 @@ appearOptions);
 faders.forEach(fader => {
   appearOnScroll.observe(fader);
 });
+
+// Javascript for videoer på proces side:
+
+let erPaaPause = true;
+
+let observer = new IntersectionObserver((entries, observer) => { 
+    for (const videosted of entries){ 
+        if (videosted.isIntersecting && erPaaPause===true) {  //Hvis videoen er i synsfelt og er på pause...
+            video1.play();
+            video1.loop = true // Videoen skal køre i loop
+            erPaaPause = false;
+        } else { // Hvis ikke... så skal videoen pause
+            video1.pause();
+            erPaaPause = true;
+        }
+    };
+
+}, {threshold:0.5}); //1 = artikel skal være 50% synlig for at tælle med
+
+// Hovedprogram
+const video1 = document.getElementById("video1"); //Observer kun på sted med id = video1
+observer.observe(video1);
+
+// Her starter video 2 med samme funktioner
+
+let erPaaPause2 = true;
+
+let observer2 = new IntersectionObserver((entries, observer) => {
+    for (const videosted of entries){ 
+        if (videosted.isIntersecting && erPaaPause2===true) { //Hvis videoen er i synsfelt og er på pause...
+            video2.play();
+            video2.loop = true
+            erPaaPause2 = false;
+        } else { // Hvis ikke... så skal videoen pause
+            video2.pause();
+            erPaaPause2 = true;
+        }
+    };
+
+}, {threshold:0.5}); //1 = artikel skal være 50% synlig for at tælle med
+
+// Hovedprogram
+const video2 = document.getElementById("video2"); //Observer kun på sted med id = video2
+observer2.observe(video2);
+
+// Her starter video 3 med samme funktioner
+
+let erPaaPause3 = true;
+
+let observer3 = new IntersectionObserver((entries, observer) => {
+    for (const entry of entries){ 
+        if (entry.isIntersecting && erPaaPause3===true) { //Hvis videoen er i synsfelt og er på pause...
+            video3.play();
+            erPaaPause3 = false;
+            video3.loop = true;
+        } else { // Hvis ikke... så skal videoen pause
+            video3.pause();
+            erPaaPause3 = true;
+        }
+    };
+
+}, {threshold:0.5}); //1 = artikel skal være 50% synlig for at tælle med
+
+// Hovedprogram
+const video3 = document.getElementById("video3"); //Observer kun på sted med id = video3
+observer3.observe(video3);
+
+// Her starter video 4 med samme funktioner
+
+let erPaaPause4 = true;
+
+let observer4 = new IntersectionObserver((entries, observer) => {
+    for (const videostart of entries){ 
+        if (videostart.isIntersecting && erPaaPause4===true) { //Hvis videoen er i synsfelt og er på pause...
+            video4.play();
+            erPaaPause4 = false;
+            video4.loop = true;
+        } else { // Hvis ikke... så skal videoen pause
+            video4.pause();
+            erPaaPause3 = true;
+        }
+    };
+
+}, {threshold:0.5}); //1 = artikel skal være 50% synlig for at tælle med
+
+// Hovedprogram
+const video4 = document.getElementById("video4"); //Observer kun på sted med id = video4
+observer4.observe(video4);
